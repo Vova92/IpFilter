4681a88ff818caf7fa801e920ce08f279e498aa1
Vova92 <vova.tat22@gmail.com>
2019-09-06 21:15:58 +0300

    asd

--- ipf.cpp
+++ ipf.cpp
@@ -1,9 +1,9 @@
 #include "ipf.h"
 
-extern bool IPF_R = 0;
-extern bool IPF_O = 0;
-extern bool IPF_F_S = 0;
-extern bool IPF_F = 0;
+bool IPF_R = 0;
+bool IPF_O = 0;
+bool IPF_F_S = 0;
+bool IPF_F = 0;
 
 bool IP_filtration_check_result(int number)
 {
